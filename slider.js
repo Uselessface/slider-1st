13638ed6efe2f59b2db8604a5be24074089e9c29
Uselessface <60576572+Uselessface@users.noreply.github.com>
2020-06-23 17:46:48 +0400

    slider

---
+++ slider.js
@@ -0,0 +1,25 @@
+var slideindex=1;
+showslides(slideindex);
+function plusslide(n){
+	showslides(slideIndex += n);
+}
+
+function currentslide(n){
+	showslides(slideIndex =n);
+}
+
+function showslides(n){
+	var i;
+	var slides = document.getElementsByClassName("slide");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {slideIndex = 1}
+  	if (n < 1) {slideIndex = slides.length}
+  	for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  	}
+  	for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  	}
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
